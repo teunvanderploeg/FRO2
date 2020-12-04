5f24d0b478fad76f55c1d3ae644df93769e09aca
Teun van der Ploeg <29253@ma-web.nl>
2020-12-04 11:44:09 +0100

    nav animatie
    nav animatie gemaakt

--- ANIMATIE/js/animatie.js
+++ ANIMATIE/js/animatie.js
@@ -7,4 +7,17 @@ document.querySelector('.animeer-knop').addEventListener('click', (e) =>{
 // na bepaalde tijd komt vlakje weer terug
 setTimeout(()=>{
     document.querySelector('.vlakje').classList.toggle('vlakje-naar-rechts');
-}, 2000)
+}, 2000);
+
+// menu klik nav__knop
+
+const menuKnop = document.querySelector('.nav__knop');
+const navItems = document.querySelectorAll('.nav__link');
+
+menuKnop.addEventListener('click', ()=>{
+    navItems.forEach((item,index) => {
+        setTimeout(()=>{
+            item.classList.toggle('nav__link--schuif-in');
+        }, 50 * index);
+    })
+});
