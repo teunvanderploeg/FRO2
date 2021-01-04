dc8de52633f52c103b35c8bad173d34676809f4a
Teun van der Ploeg <29253@ma-web.nl>
2021-01-04 12:11:15 +0100

    fire fly's
    fire fly's komen nu als je op het hard drukt

--- GreenSock/MaLogo/script.js
+++ GreenSock/MaLogo/script.js
@@ -16,6 +16,7 @@ document.getElementById('_g_').addEventListener('click', G_move);
 function love_move(){
     gsap.to('.love', { duration: 2, x: '4em', ease: 'power'});
     gsap.to('.love', {delay:1.5 , duration: 2, x: '0em', ease: 'power'});
+    gsap.to('.body', {backgroundColor: "#eeeeee", duration:1});
 }
 function A1_move(){
     gsap.to('.A1', { duration: 2, x: '4em', ease: 'power'});
@@ -59,7 +60,75 @@ function G_move(){
     gsap.to('.G', {delay:1.5 , duration: 2, x: '0em', ease: 'power'});
 }
 
+gsap.from('.svg', { duration: 1, y: '-10em', ease: 'bounce'});
+gsap.from('.svg', { duration: 1, x: '-30em', ease: 'power'});
 
 
-// gsap.from('.love', { duration: 1, y: '-2em', ease: 'bounce'});
-// gsap.from('.love', { duration: 1, x: '-20em', ease: 'power'});
+function love_move(){
+
+const count = 25;
+const blurCount = 20;
+const stage = document.querySelector(".stage");
+
+for (let i = 0; i < count; i++) {
+	setTimeout(() => {
+		makeLight(i);
+	}, 50 * i);
+}
+
+function makeLight(i) {
+	let span = document.createElement("span");
+	if (i < blurCount) {
+		span.classList.add("blur");
+	}
+	stage.appendChild(span);
+
+	gsap.set(span, {
+		left: gsap.utils.random(0, stage.offsetWidth),
+		top: gsap.utils.random(0, stage.offsetHeight),
+		scale: gsap.utils.random(.8, 1.2, .1),
+		opacity: 0
+	});
+
+	let tl = gsap.timeline({
+		paused: true,
+		onComplete: () => {
+			span.remove();
+			makeLight(i);
+		}
+	})
+
+	if (i < blurCount) {
+		tl.to(span, {
+			opacity: gsap.utils.random(.1, .2),
+			duration: .3
+		})
+		tl.to(span, {
+			x: gsap.utils.random(-100, 100),
+			y: gsap.utils.random(-100, 100),
+			duration: gsap.utils.random(5, 7, .2),
+			ease: Power0.easeNone
+		}, -.3)
+		tl.to(span, {
+			opacity: 0,
+			duration: .3
+		}, ">-.3")
+	} else {
+		tl.to(span, {
+			opacity: gsap.utils.random(.5, .8),
+			duration: .3
+		})
+		tl.to(span, {
+			x: gsap.utils.random(-40, 40),
+			y: gsap.utils.random(-40, 40),
+			duration: gsap.utils.random(5, 7, .2),
+			ease: Power0.easeNone
+		}, -.3)
+		tl.to(span, {
+			opacity: 0,
+			duration: .3
+		}, ">-.3")
+	}
+	tl.play();
+}
+}
